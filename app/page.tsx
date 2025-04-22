702994932e8c1c1b4f059111f7f25aa4af8aedc8
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-04-22 17:45:24 +0530

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,93 +1,22 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import { useState } from 'react';
 
-import { cn } from '@/lib/utils';
+import data from '@/data.json';
+import { cn, formatDate, shuffleArray, sortByDate } from '@/lib/utils';
 
-export default async function Page() {
-  const isActive = 0;
+export default function Page() {
+  const [isActive, setIsActive] = useState<number>(0);
 
-  const result = await fetch(
-    'https://raw.githubusercontent.com/arsalanansariofficial/resources/refs/heads/main/markdowns/recipes/featured.md'
+  const recipes = shuffleArray(data.recipes).slice(0, 3);
+  const posts = data.posts.sort((a, b) =>
+    sortByDate(a.publishedAt, b.publishedAt)
   );
 
   return (
     <main className="grow space-y-4 p-4">
-      <section className="container mx-auto space-y-4 md:space-y-8">
-        <h2 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
-          Featured Today
-        </h2>
-        <div className="group relative min-h-[30rem]">
-          <Image
-            fill
-            priority
-            src="/recipe.jpg"
-            alt="Recipe Name"
-            className="aspect-video rounded-md"
-          />
-          <div className="absolute inset-5 flex flex-col justify-end space-y-2 rounded-md p-4 md:space-y-4">
-            <h2 className="text-muted-foreground font-serif text-xl font-bold sm:text-3xl">
-              Recipe Name
-            </h2>
-            <p className="text-muted-foreground/70 line-clamp-3 text-xs md:text-base">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. A animi
-              ipsa minima iusto doloribus natus id, omnis pariatur totam aliquid
-              adipisci aspernatur reprehenderit optio quaerat!
-            </p>
-          </div>
-        </div>
-        <div className="mx-auto flex w-fit gap-4">
-          {Array(3)
-            .fill(true)
-            .map((_, index) => (
-              <Image
-                priority
-                width={50}
-                height={50}
-                key={index}
-                src="/recipe.jpg"
-                alt="Recipe Name"
-                className={cn(
-                  'hover:border-foreground outline-foreground hover:outline-foreground aspect-square cursor-pointer rounded-md outline-offset-4 hover:outline-2',
-                  { 'outline-foreground outline-2': isActive === index }
-                )}
-              />
-            ))}
-        </div>
-      </section>
-      <section className="container mx-auto space-y-8">
-        <header>
-          <h2 className="decoration-border/75 font-serif text-3xl font-black underline underline-offset-8">
-            Categories
-          </h2>
-        </header>
-        <main className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-4 sm:space-y-0">
-          <div className="relative min-h-50 overflow-clip rounded-md sm:min-h-70">
-            <Image fill src="/recipe.jpg" alt="Recipe Name" />
-            <h3 className="text-muted-foreground absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black">
-              Category
-            </h3>
-          </div>
-          <div className="relative min-h-50 overflow-clip rounded-md sm:min-h-70">
-            <Image fill src="/recipe.jpg" alt="Recipe Name" />
-            <h3 className="text-muted-foreground absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black">
-              Category
-            </h3>
-          </div>
-          <div className="relative min-h-50 overflow-clip rounded-md sm:min-h-70">
-            <Image fill src="/recipe.jpg" alt="Recipe Name" />
-            <h3 className="text-muted-foreground absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black">
-              Category
-            </h3>
-          </div>
-          <div className="relative min-h-50 overflow-clip rounded-md sm:min-h-70">
-            <Image fill src="/recipe.jpg" alt="Recipe Name" />
-            <h3 className="text-muted-foreground absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black">
-              Category
-            </h3>
-          </div>
-        </main>
-      </section>
       <section className="container mx-auto space-y-8">
         <header>
           <h2 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
@@ -95,21 +24,31 @@ export default async function Page() {
           </h2>
         </header>
         <main>
-          <Link
-            href="#"
-            className="space-y-1 sm:flex sm:justify-between sm:gap-4 sm:space-y-0"
-          >
-            <header className="max-w-lg sm:grow sm:space-y-1">
-              <h2 className="text-lg font-semibold">Post Title</h2>
-              <p className="text-muted-foreground line-clamp-2 text-sm font-light">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-                quae!
-              </p>
-            </header>
-            <footer>
-              <p className="text-sm font-light">20 April 2025</p>
-            </footer>
-          </Link>
+          {posts.length > 0 &&
+            posts.map(post => (
+              <Link
+                key={post.slug}
+                href={`/posts/${post.slug}`}
+                className="space-y-1 sm:flex sm:justify-between sm:gap-4 sm:space-y-0"
+              >
+                <header className="max-w-lg sm:grow sm:space-y-1">
+                  <h2 className="text-lg font-semibold">{post.title}</h2>
+                  <p className="text-muted-foreground line-clamp-2 text-sm font-light">
+                    {post.summary}
+                  </p>
+                </header>
+                <footer>
+                  <p className="text-sm font-light">
+                    {formatDate(post.publishedAt)}
+                  </p>
+                </footer>
+              </Link>
+            ))}
+          {posts.length === 0 && (
+            <p className="text-muted-foreground text-sm font-light">
+              No posts currently.
+            </p>
+          )}
         </main>
         <footer>
           <Link
@@ -120,32 +59,117 @@ export default async function Page() {
           </Link>
         </footer>
       </section>
-      <section className="container mx-auto space-y-4">
-        <header className="space-y-8">
-          <h2 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
-            Featured Post
+      <section className="container mx-auto space-y-4 md:space-y-8">
+        <h2 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
+          Featured Today
+        </h2>
+        <div className="group relative min-h-[30rem]">
+          <Image
+            fill
+            priority
+            suppressHydrationWarning
+            alt={recipes[isActive].slug}
+            src={recipes[isActive].image}
+            className="aspect-video rounded-md object-cover grayscale"
+          />
+          <div className="absolute inset-1 flex flex-col justify-end space-y-2 rounded-md p-4 md:space-y-4">
+            <h2
+              suppressHydrationWarning
+              className="font-serif text-xl font-bold text-white sm:text-3xl"
+            >
+              {recipes[isActive].title}
+            </h2>
+            <p
+              suppressHydrationWarning
+              className="line-clamp-3 text-xs text-white/75 md:text-base"
+            >
+              {recipes[isActive].summary}
+            </p>
+          </div>
+        </div>
+        <div className="mx-auto flex w-fit gap-4">
+          {recipes.map((recipe, index) => (
+            <Image
+              priority
+              width={50}
+              height={50}
+              key={index}
+              alt={recipe.slug}
+              src={recipe.image}
+              suppressHydrationWarning
+              onClick={() => setIsActive(index)}
+              className={cn(
+                'hover:border-foreground outline-foreground hover:outline-foreground aspect-square cursor-pointer rounded-md outline-offset-4 grayscale hover:outline-2',
+                { 'outline-foreground outline-2': isActive === index }
+              )}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="container mx-auto space-y-8">
+        <header>
+          <h2 className="decoration-border/75 font-serif text-3xl font-black underline underline-offset-8">
+            Categories
           </h2>
-          <div className="relative h-80">
+        </header>
+        <main className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-4 sm:space-y-0">
+          <Link
+            href="/recipes?category=veg"
+            className="relative min-h-50 overflow-clip rounded-md sm:min-h-70"
+          >
             <Image
               fill
-              priority
-              alt="Post Image"
-              src="/recipe.jpg"
-              className="aspect-video rounded-lg"
+              alt="Vegetables"
+              className="grayscale"
+              src="https://raw.githubusercontent.com/arsalanansariofficial/resources/refs/heads/main/images/recipes/veg.jpg"
             />
-          </div>
-        </header>
-        <main className="space-y-4">
-          <h1 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
-            Recipe Title
-          </h1>
-          <p className="text-muted-foreground text-xs">
-            Author Name / 20 April 2025
-          </p>
+            <h3 className="absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black text-white/75">
+              Veg
+            </h3>
+          </Link>
+          <Link
+            href="/recipes?category=non-veg"
+            className="relative min-h-50 overflow-clip rounded-md sm:min-h-70"
+          >
+            <Image
+              fill
+              alt="Non-vegetables"
+              className="grayscale"
+              src="https://raw.githubusercontent.com/arsalanansariofficial/resources/refs/heads/main/images/recipes/non-veg.jpg"
+            />
+            <h3 className="absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black text-white/75">
+              Non-Veg
+            </h3>
+          </Link>
+          <Link
+            href="/recipes?category=snacks"
+            className="relative min-h-50 overflow-clip rounded-md sm:min-h-70"
+          >
+            <Image
+              fill
+              src="https://raw.githubusercontent.com/arsalanansariofficial/resources/refs/heads/main/images/recipes/snacks.jpg"
+              alt="Snacks"
+              className="grayscale"
+            />
+            <h3 className="absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black text-white/75">
+              Snacks
+            </h3>
+          </Link>
+          <Link
+            href="/recipes?category=beverage"
+            className="relative min-h-50 overflow-clip rounded-md sm:min-h-70"
+          >
+            <Image
+              fill
+              alt="Beverages"
+              src="https://raw.githubusercontent.com/arsalanansariofficial/resources/refs/heads/main/images/recipes/beverage.jpg"
+              className="grayscale"
+            />
+            <h3 className="absolute inset-x-4 bottom-4 min-h-fit font-serif text-2xl font-black text-white/75">
+              Beverages
+            </h3>
+          </Link>
         </main>
-        <footer className="prose dark:prose-invert max-w-none">
-          <MDXRemote source={await result.text()} />
-        </footer>
       </section>
     </main>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -12,6 +12,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function sortByDate(a: string, b: string) {
+  return new Date(b).getTime() - new Date(a).getTime();
+}
+
 export function getMarkDown(info: Info) {
   const { name, summary, ingredients, steps } = info;
   return `# ${name}\n\n${summary}\n\n## Ingredients\n\n${(ingredients as string[]).map(ing => `- ${ing}`)}\n\n## Steps\n\n${(steps as string[]).map(step => `- ${step}`)}`;
@@ -21,4 +25,23 @@ export function getFileName(slug: string, extension: string): string {
   const timestamp = Date.now();
   const randomString = Math.random().toString(36).substring(2, 8);
   return `${slug}-${timestamp}-${randomString}.${extension}`;
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  });
+}
+
+export function shuffleArray<T>(array: T[]) {
+  const shuffledArray = array.slice();
+
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
 }
