c5cffdf61bba55ca56769f1d79a9e33852076978
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-04-23 06:21:56 +0530

    Update posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,20 @@
-import Link from 'next/link';
+'use client';
 
+import Link from 'next/link';
+import { useState } from 'react';
+
+import data from '@/data.json';
+import { formatDate } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
 export default function Page() {
+  const [filter, setFilter] = useState<string>(String());
+
+  const posts = data.posts.filter(post =>
+    post.title.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <main className="grow p-4">
       <section className="container mx-auto space-y-4">
@@ -11,31 +22,49 @@ export default function Page() {
           <h1 className="decoration-border/75 font-serif text-3xl font-bold underline decoration-2 underline-offset-8">
             Posts
           </h1>
-          <div className="flex gap-2">
-            <Input type="text" placeholder="Search posts..." />
-            <Button variant="secondary">Reset</Button>
-          </div>
+          <form className="flex gap-2">
+            <Input
+              type="text"
+              placeholder="Search posts..."
+              onChange={e => setFilter(e.target.value)}
+            />
+            <Button
+              type="reset"
+              variant="secondary"
+              onClick={() => setFilter(String())}
+            >
+              Reset
+            </Button>
+          </form>
         </header>
         <main>
-          <ul>
-            <li>
-              <Link
-                href="posts/featured"
-                className="space-y-1 sm:flex sm:justify-between sm:gap-4 sm:space-y-0"
-              >
-                <header className="max-w-lg sm:grow sm:space-y-1">
-                  <h2 className="text-lg font-semibold">Post Title</h2>
-                  <p className="text-muted-foreground line-clamp-2 text-sm font-light">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Veritatis hic corporis autem dolorem, quibusdam aut.
-                  </p>
-                </header>
-                <footer>
-                  <p className="text-sm font-light">20 April 2025</p>
-                </footer>
-              </Link>
-            </li>
-          </ul>
+          {posts.length === 0 && (
+            <p className="text-muted-foreground">No posts currently.</p>
+          )}
+          {posts.length > 0 && (
+            <ul>
+              {posts.map(post => (
+                <li key={post.slug}>
+                  <Link
+                    href={`/posts/${post.slug}`}
+                    className="space-y-1 sm:flex sm:justify-between sm:gap-4 sm:space-y-0"
+                  >
+                    <header className="max-w-lg sm:grow sm:space-y-1">
+                      <h2 className="text-lg font-semibold">{post.title}</h2>
+                      <p className="text-muted-foreground line-clamp-2 text-sm font-light">
+                        {post.summary}
+                      </p>
+                    </header>
+                    <footer>
+                      <p className="text-sm font-light">
+                        {formatDate(post.publishedAt)}
+                      </p>
+                    </footer>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </main>
       </section>
     </main>
